4355543f0f011be601d6a2d6d46c9ea33502a055
Franco <franangh.20@gmail.com>
2023-07-12 19:43:44 -0300

    proyecto-cascada

---
+++ proyecto-cascada.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+¿en que tipo de proyectos es conveniente utilizar el desarrollo en cascada?
+
+En si se podria usar en proyectos a largo plazo pero no es conveniente porque
+muchas veces al cliente le van surgiendo otras ideas y necesita otro tipo de requisitos que son muy dificiles de satisfacer
+por eso es mejor usarlo en proyectos a corto plazo.
+
+
+
+*/
+
+?>
